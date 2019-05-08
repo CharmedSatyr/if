c8d690127507e6939362545f1383a6dce61ee219
Joseph <z@charmed.tech>
2019-05-08 12:16:00 -0700

    Simplify back down...

--- if.js
+++ if.js
@@ -1,6 +1,11 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
-const If = props => props.condition ? React.createElement(Fragment, null, props.then || props.children) : React.createElement(Fragment, null, props.unless || props.else || null);
+const If = props =>
+  props.condition ? (
+    <Fragment>{props.then || props.children}</Fragment>
+  ) : (
+    <Fragment>{props.unless || props.else || null}</Fragment>
+  );
 
-ReactDOM.render(React.createElement(If, null), document.getElementById('root'));
+export default If;
