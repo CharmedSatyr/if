345abc9033bdf1de9921941b446554004df68c2b
Joseph <z@charmed.tech>
2019-05-09 12:31:25 -0700

    Tighten conditional statements, provide fallback

--- src/if.js
+++ src/if.js
@@ -1,10 +1,20 @@
 import React, { Fragment } from 'react';
 
-const If = props =>
-  props.condition ? (
-    <Fragment>{props.then || props.children}</Fragment>
-  ) : (
-    <Fragment>{props.unless || props.else || null}</Fragment>
-  );
+const If = props => {
+  if (!!props.condition) {
+    if (!!props.then) {
+      return <Fragment>{props.then}</Fragment>;
+    } else if (!!props.children) {
+      return <Fragment>{props.children}</Fragment>;
+    }
+  } else {
+    if (!!props.unless) {
+      return <Fragment>{props.unless}</Fragment>;
+    } else if (!!props.else) {
+      return <Fragment>{props.else}</Fragment>;
+    }
+  }
+  return null;
+};
 
 export default If;
